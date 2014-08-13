6d6e099ad23f5d606c70302f002308f19dc0d6df
Andrew Cadman <archictectureandvirtuality@gmail.com>
2014-08-13 08:49:45 +0100

    Clean up of feature steps

--- cucumber-bdd/features/step_definitions/wraith_steps.rb
+++ cucumber-bdd/features/step_definitions/wraith_steps.rb
@@ -8,23 +8,26 @@ Given(/^I wish to create and compare screenshots of URLs using (.*)$/) do |drive
   @driver = driver
 end
 
-And(/^I wish to use the engine mode (.*)$/) do |mode|
+And(/^I wish to use the engine mode (.*) with (.*) as a base comparison$/) do |mode,base_type|
   @mode = mode
-end
-
-And(/^I wish to use a (.*) as a base comparison in all cases$/) do |base_type|
   @base_type = base_type
 end
 
+# And(/^I wish to use a (.*) as a base comparison in all cases$/) do |base_type|
+#   @base_type = base_type
+# end
 
-And(/^I want to test a (.*)$/) do |page_or_component|
+
+And(/^I want to test a (.*) on (.*)$/) do |page_or_component,device_or_desktop|
   @page_or_component = page_or_component
-end
-
-And(/^I wish to test on (.*)$/) do |device_or_desktop|
   @config_file = 'test' + '_' + @driver + '_' +  @mode + '_' + @base_type + '_' + @page_or_component  + '_' + device_or_desktop + '_config'
   @cnf_vals = YAML::load(File.open('configs/' + @config_file + '.yaml'))
 end
+
+# And(/^I wish to test on (.*)$/) do |device_or_desktop|
+#   @config_file = 'test' + '_' + @driver + '_' +  @mode + '_' + @base_type + '_' + @page_or_component  + '_' + device_or_desktop + '_config'
+#   @cnf_vals = YAML::load(File.open('configs/' + @config_file + '.yaml'))
+# end
 
 When(/^I capture screen shots for a list of browser widths$/) do
   @cli = Wraith::CLI.new
@@ -56,69 +59,77 @@ And(/^I create thumbnails and a gallery$/) do
   @cli.generate_gallery(@config_file)
 end
 
-Then(/^I expect to see (.*) (.*) files preserved for each width for (.*)$/) do |expected_file_count,file_type,device_or_desktop|
+# Then(/^I expect to see (.*) (.*) files preserved for each width for (.*)$/) do |expected_file_count,file_type,device_or_desktop|
+Then(/^I expect to see (.*) base, (.*) compare, (.*) diff, (.*) data files preserved for each width for (.*)$/) do |exp_base_cnt,exp_compare_cnt,exp_data_cnt,exp_diff_cnt,device_or_desktop|
 
-  extn = '.png'
-  if file_type == 'data'
-    extn = '.txt'
-  end
+  file_type_counts = Hash.new()
+  file_type_counts[:base] = exp_base_cnt
+  file_type_counts[:compare] = exp_compare_cnt
+  file_type_counts[:data] = exp_data_cnt
+  file_type_counts[:diff] = exp_diff_cnt
+
   shot_directory = @cnf_vals['directory'][0]
   screen_properties = @cnf_vals['screen_properties']
   paths = @cnf_vals['paths']
 
-   paths.each_key do |path|
-    full_path = Dir.pwd + '/' + shot_directory + '/' + path
-    screen_properties[device_or_desktop].each do |props|
+  file_type_counts.each do |key,value|
+    file_type = key.to_s
+    file_type == 'data' ? extn = '.txt' : extn = '.png'
 
-      props.is_a?(Hash) ? width = props[:name] : width = props
+    paths.each_key do |path|
+      full_path = Dir.pwd + '/' + shot_directory + '/' + path
+      screen_properties[device_or_desktop].each do |props|
 
-      regex_hash = {
-        'prefix' => width,
-        'middle' => '',
-        'suffix' => file_type
-      }
-      count = helpers.file_count(full_path, regex_hash, extn, 100, 2, 'count')
-      count.to_s.should == expected_file_count
+        props.is_a?(Hash) ? width = props[:name] : width = props
+
+        regex_hash = {
+          'prefix' => width,
+          'middle' => '',
+          'suffix' => file_type
+        }
+        count = helpers.file_count(full_path, regex_hash, extn, 100, 2, 'count')
+        expect(count.to_s).to eq value
+      end
     end
   end
 end
 
-And(/^the filename of the image should reflect that it was created using (.*)$/) do |driver|
+# And(/^the filename of the image should reflect that it was created using (.*)$/) do |driver|
+#
+#   extn = '.png'
+#   shot_directory = @cnf_vals['directory'][0]
+#   paths = @cnf_vals['paths']
+#   regex_hash = {
+#     'prefix' => '',
+#     'middle' => driver,
+#     'suffix' => ''
+#   }
+#
+#   paths.each_key do |path|
+#     full_path = Dir.pwd + '/' + shot_directory + '/' + path
+#     result = helpers.file_count(full_path, regex_hash, extn, 100, 0.5, 'required')
+#     expect(result).to eq true
+#   end
+# end
 
-  extn = '.png'
-  shot_directory = @cnf_vals['directory'][0]
-  paths = @cnf_vals['paths']
-  regex_hash = {
-    'prefix' => '',
-    'middle' => driver,
-    'suffix' => ''
-  }
-
-  paths.each_key do |path|
-    full_path = Dir.pwd + '/' + shot_directory + '/' + path
-    result = helpers.file_count(full_path, regex_hash, extn, 100, 0.5, 'required')
-    result.should == true
-  end
-end
-
-And(/^the filename of the image should reflect whether it was created using (.*)$/) do  |property|
+And(/^the filename of the image should reflect that it was created using (.*), (.*), (.*) and (.*)$/) do  |driver,mode,base_type,device_or_desktop|
   expected_browser_device_file_count = test_expectations['base_type_browser_device_file_count']
+  extn = '.png'
   shot_directory = @cnf_vals['directory'][0]
   paths = @cnf_vals['paths']
   #reverse mapping of specified driver to the suite which maps to the driver in the config file
   browser_suite  = @cnf_vals['suites'][test_expectations['driver_suites'][@driver]]
-
   browser_suite.each do |browser|
     regex_hash = {
       'prefix' => '',
-      'middle' => property,
+      'middle' => base_type,
       'suffix' => browser
     }
 
     paths.each_key do |path|
       full_path = Dir.pwd + '/' + shot_directory + '/' + path
       result = helpers.file_count(full_path, regex_hash, '.png', 100, 1.0, 'count')
-      result.to_i.should == expected_browser_device_file_count[@mode][@base_type][browser][property].to_i
+      expect(result.to_i).to eq expected_browser_device_file_count[mode][base_type][browser][device_or_desktop].to_i
     end
   end
 end
@@ -126,7 +137,7 @@ end
 And(/^a gallery of images created as an HTML page$/) do
   shot_directory = @cnf_vals['directory'][0]
   gallery_path = Dir.pwd + '/' + shot_directory +'/gallery.html'
-  File.file?(gallery_path).should == true
+  expect(File.file?(gallery_path)).to eq true
 end
 
 And(/^the gallery page should contain the parameters used as information$/) do
@@ -153,7 +164,7 @@ And(/^a thumbnail version should be created for the images at each width giving 
         'suffix' => ''
       }
       count = helpers.file_count(full_path, regex_hash, extn, 100, 2, 'count')
-      count.to_s.should == thumbnail_count
+      expect(count.to_s).to == thumbnail_count
     end
   end
 end
--- cucumber-bdd/features/wraith.feature
+++ cucumber-bdd/features/wraith.feature
@@ -2,24 +2,16 @@ Feature: Capturing browser screenshots of URLS and calculating diffs between a b
 
   Scenario Outline: 1: Using a given browser driver and comparing screen shots against a base image
     Given I wish to create and compare screenshots of URLs using <driver>
-    And I wish to use the engine mode <mode>
-    And I wish to use a <base_type> as a base comparison in all cases
-    And I want to test a <page_or_component>
-    And I wish to test on <device_or_desktop>
+    And I wish to use the engine mode <mode> with <base_type> as a base comparison
+    And I want to test a <page_or_component> on <device_or_desktop>
     When I capture screen shots for a list of browser widths
     And I crop the screen shots
     And I do diffs of the screen shots against the base image
     And I create thumbnails and a gallery
-    Then I expect to see <base_image_count> base files preserved for each width for <device_or_desktop>
-    And I expect to see <compare_image_count> compare files preserved for each width for <device_or_desktop>
-    And I expect to see <diff_image_count> diff files preserved for each width for <device_or_desktop>
-    And I expect to see <data_file_count> data files preserved for each width for <device_or_desktop>
-    And the filename of the image should reflect that it was created using <driver>
-    And the filename of the image should reflect that it was created using <mode>
-    And the filename of the image should reflect whether it was created using <device_or_desktop>
+    Then I expect to see <base_image_count> base, <compare_image_count> compare, <diff_image_count> diff, <data_file_count> data files preserved for each width for <device_or_desktop>
+    And the filename of the image should reflect that it was created using <driver>, <mode>, <base_type> and <device_or_desktop>
     And a thumbnail version should be created for the images at each width giving <thumbnail_count> images per width for <device_or_desktop>
     And a gallery of images created as an HTML page
-    And the gallery page should contain the parameters used as information
   Examples:
     |driver       |mode   |base_type  |page_or_component  |base_image_count |compare_image_count|diff_image_count |data_file_count|thumbnail_count|device_or_desktop|
     #phantomJS url based page comparison
